7f6433aed01921341bd3e4f2e07b811ec469b4e4
Alero Awani <aleroawani18@gmail.com>
2025-03-03 23:21:52 +0100

    create local files for helm values

--- argocd.tf
+++ argocd.tf
@@ -1,56 +1,56 @@
-# locals {
-#   master_utils_values = <<-EOT
-#   apiVersion: argoproj.io/v1alpha1
-#   kind: Application
-#   metadata:
-#     name: master-utilities
-#     finalizers:
-#       - resources-finalizer.argocd.argoproj.io
-#   spec:
-#     project: default
-#     source:
-#       repoURL: https://github.com/PacktPublishing/ArgoCD-in-Practice.git
-#       targetRevision: HEAD
-#       path: ch05/applications/master-utilities
-#       helm:
-#         values: |
-#           externalDNS:
-#             iamRole: ${aws_iam_role.external_dns.arn}
-#             domain: ${var.domain}
-#             txtOwnerID: ${var.zone_id}
+locals {
+  master_utils_values = <<-EOT
+  apiVersion: argoproj.io/v1alpha1
+  kind: Application
+  metadata:
+    name: master-utilities
+    finalizers:
+      - resources-finalizer.argocd.argoproj.io
+  spec:
+    project: default
+    source:
+      repoURL: https://github.com/PacktPublishing/ArgoCD-in-Practice.git
+      targetRevision: HEAD
+      path: ch05/applications/master-utilities
+      helm:
+        values: |
+          externalDNS:
+            iamRole: ${aws_iam_role.external_dns.arn}
+            domain: ${var.domain}
+            txtOwnerID: ${var.zone_id}
 
-#     destination:
-#       namespace: argocd
-#       server: https://kubernetes.default.svc
+    destination:
+      namespace: argocd
+      server: https://kubernetes.default.svc
 
-#     syncPolicy: 
-#       automated:
-#         prune: true
-#         selfHeal: true
+    syncPolicy: 
+      automated:
+        prune: true
+        selfHeal: true
 
-#   EOT
+  EOT
 
-#   values_dev = <<-EOT
-#   spec:
-#     destination:
-#       server: https://kubernetes.default.svc
+  values_dev = <<-EOT
+  spec:
+    destination:
+      server: https://kubernetes.default.svc
 
-#   externalDNS:
-#     iamRole: ${aws_iam_role.external_dns.arn}
-#     domain: ${var.domain}
-#     txtOwnerID: ${var.zone_id}
-#   EOT
-# }
+  externalDNS:
+    iamRole: ${aws_iam_role.external_dns.arn}
+    domain: ${var.domain}
+    txtOwnerID: ${var.zone_id}
+  EOT
+}
 
-# resource "local_file" "master_utils_values" {
-#   filename = "./k8s-bootstrap/base/master-utilities.yaml"
-#   content  = local.master_utils_values
-# }
+resource "local_file" "master_utils_values" {
+  filename = "./k8s-bootstrap/base/master-utilities.yaml"
+  content  = local.master_utils_values
+}
 
-# resource "local_file" "master_utils_values_yaml" {
-#   filename = "./applications/master-utilities/values.yaml"
-#   content  = local.values_dev
-# }
+resource "local_file" "master_utils_values_yaml" {
+  filename = "./applications/master-utilities/values.yaml"
+  content  = local.values_dev
+}
 
 data "kustomization_build" "argocd_namespace" {
   path = "./k8s-bootstrap/namespace"
@@ -62,9 +62,9 @@ resource "kustomization_resource" "argocd_namespace" {
   depends_on = [null_resource.kubectl]
 
 }
-resource "time_sleep" "wait_180_seconds" {
+resource "time_sleep" "wait_60_seconds" {
   depends_on      = [kustomization_resource.argocd_namespace]
-  create_duration = "180s"
+  create_duration = "60s"
 }
 
 data "kustomization_build" "argocd" {
@@ -74,7 +74,7 @@ data "kustomization_build" "argocd" {
 resource "kustomization_resource" "argocd" {
   for_each = data.kustomization_build.argocd.ids
   manifest = data.kustomization_build.argocd.manifests[each.value]
-  depends_on = [time_sleep.wait_180_seconds]
+  depends_on = [time_sleep.wait_60_seconds]
   # depends_on = [
   #   local_file.master_utils_values,
   #   local_file.master_utils_values_yaml,
